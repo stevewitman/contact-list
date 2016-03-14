aa04e4ee9e79f58bc21cc0230280cc75f07836db
Steve Witman <stevewitman@gmail.com>
2016-03-13 23:34:31 -0600

    added edit contact functionality

--- app/contacts/contacts.js
+++ app/contacts/contacts.js
@@ -14,6 +14,18 @@ angular.module('myApp.contacts', ['ngRoute'])
 
   $scope.contacts = $firebaseArray(ref);
 
+  $scope.addFormShow = true;
+  $scope.editFormShow = false;
+
+  $scope.showEditForm = function(contact) {
+    $scope.addFormShow = false;
+    $scope.editFormShow = true;
+    $scope.id = contact.$id;
+    $scope.name = contact.name;
+    $scope.email = contact.email;
+    $scope.phone = contact.phone;
+  }
+
   $scope.addContact = function() {
     $scope.contacts.$add({
       name: $scope.name,
@@ -25,6 +37,20 @@ angular.module('myApp.contacts', ['ngRoute'])
       $scope.email = '';
       $scope.phone = '';
     });
+  }
+
+  $scope.editContact = function(contact) {
+    var id = $scope.id;
+    var record = $scope.contacts.$getRecord(id);
+    record.name = $scope.name;
+    record.email = $scope.email;
+    record.phone = $scope.phone;
+    $scope.contacts.$save(record).then(function(ref) {
+      console.log(ref.key);
+      $scope.name = '';
+      $scope.email = '';
+      $scope.phone = '';
+    })
   }
 
   $scope.removeContact = function(contact) {
